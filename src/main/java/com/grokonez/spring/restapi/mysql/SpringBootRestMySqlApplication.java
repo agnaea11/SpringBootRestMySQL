017bae1e32103e6de2fae23a8b05fd700f2f4fb0
agna.ea <agna.ea@claysys.net>
2021-09-22 14:10:33 +0530

    Initial commit

---
+++ src/main/java/com/grokonez/spring/restapi/mysql/SpringBootRestMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.grokonez.spring.restapi.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestMySqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestMySqlApplication.class, args);
+		System.out.println("welcome");
+	}
+}
